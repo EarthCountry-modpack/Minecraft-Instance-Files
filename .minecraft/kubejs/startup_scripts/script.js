fcb1c5cc0b273ec6ab9b3fe55357ddca1831eb6b
Nikki Devil <nikkidevil.rei@gmail.com>
2023-04-21 16:36:12 +0200

    Added Quark textures Added Quark Housing

--- .minecraft/kubejs/startup_scripts/script.js
+++ .minecraft/kubejs/startup_scripts/script.js
@@ -9,12 +9,13 @@ onEvent('item.registry', event => {
 
 	
 	// Particles Items
-	event.create('quark1_up').displayName('§3Up quark§r')
-	event.create('quark1_down').displayName('§3Down quark§r')
-	event.create('quark2_charm').displayName('§3Charm quark§r')
-	event.create('quark2_strange').displayName('§3Strange quark§r')
-	event.create('quark3_top').displayName('§3Top quark§r')
-	event.create('quark3_bottom').displayName('§3Bottom quark§r')
+	event.create('quark0_housing').displayName('§3Quark Housing§r')
+	event.create('quark1_up').displayName('§3Quark Up§r')
+	event.create('quark1_down').displayName('§3Quark Down§r')
+	event.create('quark2_charm').displayName('§3Quark Charm§r')
+	event.create('quark2_strange').displayName('§3Quark Strange§r')
+	event.create('quark3_top').displayName('§3Quark Top§r')
+	event.create('quark3_bottom').displayName('§3Quark Bottom§r')
 
 })
 
